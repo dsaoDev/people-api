c08a3344efb9d908ecdf277c54fda18f451b096d
dsaoDev <davissgt1@hotmail.com>
2023-05-22 22:14:45 -0300

    projeto criado

---
+++ src/main/java/com/dsaoDev/peopleAPI/PeopleApiApplication.java
@@ -0,0 +1,13 @@
+package com.dsaoDev.peopleAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeopleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeopleApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dsaoDev/peopleAPI/PeopleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsaoDev.peopleAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeopleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
