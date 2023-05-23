fdc8a0bf9bb9401aa047e685b2d29a0dd8ad99d7
dsaoDev <davissgt1@hotmail.com>
2023-05-23 20:29:31 -0300

    todos os endpoints testados e funcional, iniciando validação e tratamento de erro

--- src/main/java/com/dsaoDev/peopleAPI/controllers/PersonController.java
+++ src/main/java/com/dsaoDev/peopleAPI/controllers/PersonController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,7 +40,7 @@ public class PersonController {
 		return ResponseEntity.ok(personService.findById(id));
 	}
 	
-	@PostMapping(value = "/{id}")
+	@PutMapping(value = "/{id}")
 	public ResponseEntity<PersonResponseDTO> update (@RequestBody PersonRequestDTO personDTO, @PathVariable(name = "id")Long id){
 		return ResponseEntity.ok(personService.update(personDTO, id));
 	}
