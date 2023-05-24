f0fc327aea18986bbf717eea320e5ee5ca8c3cbf
dsaoDev <davissgt1@hotmail.com>
2023-05-24 01:17:15 -0300

    mudança na logica do enum,foi implementando no application properties o comando de linha spring.jackson.mapper.accept-case-insensitive-enums=true que tem o mesmo efeito da logica feita no enum anteriormente

--- src/main/java/com/dsaoDev/peopleAPI/entities/enums/Genero.java
+++ src/main/java/com/dsaoDev/peopleAPI/entities/enums/Genero.java
@@ -1,27 +1,7 @@
 package com.dsaoDev.peopleAPI.entities.enums;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonValue;
-
 public enum Genero {
-	MASCULINO("masculino"),
-	FEMININO("feminino");
-	
-	private String key;
-	
-	Genero(String key){
-		this.key = key;
-	}
-	
-	@JsonCreator
-	public static Genero fromString(String key) {
-		return key == null
-				?null
-				:Genero.valueOf(key.toUpperCase());
-	}
-	@JsonValue
-	public String getKey() {
-		return key;
-	}
+	MASCULINO,
+	FEMININO;
 	
 }
